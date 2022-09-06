87b325be4edde0b224d6dcf8f74f969d5b1ad595
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-09-06 23:27:29 +0200

    googd

--- public/wordpress/js/index.set.images.js
+++ public/wordpress/js/index.set.images.js
@@ -11,10 +11,10 @@ div_images_gallery.innerHTML = content_page
 function put_images_in_page(i,item){
     return `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
             <figure class="effect-ming tm-video-item">
-            <img src="/wordpress/skrep-img/`+item.image+`" alt="Image" class="img-fluid">
+            <img src="/wordpress/skrep-img/`+item.image+`" alt="`+item.title+`" class="img-fluid">
             <figcaption class="d-flex align-items-center justify-content-center">
             <h2>`+item.title+`</h2>
-            <a href="#/image-`+i+`">View more</a>
+            <a href="#image-`+i+`">`+item.title+`</a>
             </figcaption>                    
             </figure>
             <div class="d-flex justify-content-between tm-text-gray">
