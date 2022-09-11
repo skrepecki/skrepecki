90458dd5bb7f3fc1d42a2e8e9c367954dab5a9d2
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-09-11 11:06:55 +0200

    jijijjijjijijijiji9j9j9j

--- public/wordpress/js/image.jcustom.js
+++ public/wordpress/js/image.jcustom.js
@@ -2,13 +2,14 @@
 
 function set_custom_image_top(image_id){
     let img_html = document.getElementById('img_html')
-        img_html.src = '/wordpress/skrep-img/' + skrep_storage[image_id].image
+        img_html.src = 'https://diseno-web-cantabria.github.io/skrepeckiy.web.app/public/wordpress/skrep-img/' + skrep_storage[image_id].image
+        img_html.alt = skrep_storage[image_id].title + ' - Семен Скрепецкий - Скрепоносный Бузотер'
     let title_custom_image = document.getElementById('title_custom_image')
         title_custom_image.innerText = skrep_storage[image_id].title  
     let image_description = document.getElementById('image_description')
         image_description.innerText = skrep_storage[image_id].description
     let download_link = document.getElementById('download_link')
-        download_link.href = 'https://skrepeckiy.web.app/wordpress/skrep-img/'+skrep_storage[image_id].image  
+        download_link.href = 'https://diseno-web-cantabria.github.io/skrepeckiy.web.app/public/wordpress/skrep-img/'+skrep_storage[image_id].image  
         download_link.download = ''+skrep_storage[image_id].image        
 }
 
@@ -18,24 +19,26 @@ function set_random_images_bottom(){
     let custom_image_child_items = document.getElementById('custom_image_child_items')
     let result_children_items = ''
     for(let i = 0; i < array_rnd_int.length; i++){ result_children_items += create_custom_child_item(array_rnd_int[i]) }
-    // setTimeout(()=> {
         custom_image_child_items.innerHTML = result_children_items
-    // }, 11000)
+    
    
 }
 
 function create_custom_child_item(id){
     return `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
             <figure class="effect-ming tm-video-item">
-                <img src="/wordpress/skrep-img/`+skrep_storage[id].image+`" alt="`+skrep_storage[id].title+`" class="img-fluid index_page_list_image">
+                <img src="https://diseno-web-cantabria.github.io/skrepeckiy.web.app/public/wordpress/skrep-img/`+skrep_storage[id].image+`" alt="`+skrep_storage[id].title+`" class="img-fluid index_page_list_image">
                 <figcaption class="d-flex align-items-center justify-content-center">
                     <h2>`+skrep_storage[id].title+`</h2>
                     <a href="#image-`+id+`">`+skrep_storage[id].title+`</a>
                 </figcaption>                    
             </figure>
             <div class="d-flex justify-content-between tm-text-gray">
-                <span class="tm-text-gray-light">16 Oct 2020</span>
-                <span>12,460 views</span>
+                <span class="tm-text-gray-light"></span>
+                <span></span>
             </div>
             </div>`
 }
+
+
+// https://diseno-web-cantabria.github.io/skrepeckiy.web.app/public/wordpress/skrep-img
--- public/wordpress/js/index.set.images.js
+++ public/wordpress/js/index.set.images.js
@@ -13,15 +13,15 @@ div_images_gallery.innerHTML = content_page
 function put_images_in_page(i,item){
     return `<div class="col-xl-3 col-lg-4 col-md-6 col-sm-6 col-12 mb-5">
             <figure class="effect-ming tm-video-item">
-            <img src="/wordpress/skrep-img/`+item.image+`" alt="`+item.title+`" class="img-fluid index_page_list_image">
+            <img src="https://diseno-web-cantabria.github.io/skrepeckiy.web.app/public/wordpress/skrep-img/`+item.image+`" alt="`+item.title+`" class="img-fluid index_page_list_image">
             <figcaption class="d-flex align-items-center justify-content-center">
             <h2>`+item.title+`</h2>
             <a href="#image-`+i+`">`+item.title+`</a>
             </figcaption>                    
             </figure>
             <div class="d-flex justify-content-between tm-text-gray">
-            <span class="tm-text-gray-light">18 Oct 2020</span>
-            <span>9,906 views</span>
+            <span class="tm-text-gray-light"></span>
+            <span></span>
             </div>
             </div>`
 }
