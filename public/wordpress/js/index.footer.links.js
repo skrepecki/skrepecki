b8132e943c808bc27730a4d5260f727536536bb8
alexei <alexei.saron@gmail.com>
2022-09-12 10:44:41 +0200

    jijijijj

--- public/wordpress/js/index.footer.links.js
+++ public/wordpress/js/index.footer.links.js
@@ -6,5 +6,5 @@ for(let i = 0; i < skrep_storage.length; i++) html_links_result += return_one_li
 footer_list_links.innerHTML = html_links_result
 
 function return_one_link(i, title){
-    return `<div class="col-lg-3 col-md-3 col-sm-6 col-6"><a href="https://skrepeckiy.eu/#image-`+i+`" class="tm-text-gray d-block mb-2" target="_blank">`+title+`</a></div>`
+    return `<div class="col-lg-3 col-md-3 col-sm-6 col-6"><a href="/#image-`+i+`" class="tm-text-gray d-block mb-2" target="_blank">`+title+`</a></div>`
 }
