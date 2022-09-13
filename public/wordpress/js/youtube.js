9332c92aeb0bca540be17a7b0ba56f2c8ef0355d
alexei <alexei.saron@gmail.com>
2022-09-13 16:51:09 +0200

    jijijijijijjii

--- public/wordpress/js/youtube.js
+++ public/wordpress/js/youtube.js
@@ -9,6 +9,6 @@ let youtube_array = [
     'https://www.youtube.com/embed/fa28cdnpG04',
 ]
 
-for(let i = 0; i < youtube_array.length; i++) youtube_content += '<iframe width="100%" height="400px" src="'+youtube_array[i]+'"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+for(let i = 0; i < youtube_array.length; i++) youtube_content += '<iframe width="100%" height="400px" src="'+youtube_array[i]+'" title="Семен Скрепецкий - Семен Скрепецкий Картины - Скрепоносный Бузотер" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 
 document.getElementById('inframe_video_section').innerHTML = youtube_content
