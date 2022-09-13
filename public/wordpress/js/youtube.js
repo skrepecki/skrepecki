453894eba94ff159e39031631dda96a5759e7890
alexei <alexei.saron@gmail.com>
2022-09-13 16:47:26 +0200

    JijijijjijJJJJ

--- public/wordpress/js/youtube.js
+++ public/wordpress/js/youtube.js
@@ -1,8 +1,12 @@
 let youtube_content = ''
 
 let youtube_array = [
-    'https://www.youtube.com/embed/fa28cdnpG04',
+
+
+
     
+    'https://www.youtube.com/embed/LVrbQFCXHTw',
+    'https://www.youtube.com/embed/fa28cdnpG04',
 ]
 
 for(let i = 0; i < youtube_array.length; i++) youtube_content += '<iframe width="100%" height="400px" src="'+youtube_array[i]+'"  frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
