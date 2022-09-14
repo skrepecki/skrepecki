6b4510122cc3cd63a61bc652a9114256ccf283c6
alexei <alexei.saron@gmail.com>
2022-09-14 10:46:34 +0200

    jijooo

--- public/wordpress/js/register-form-html.js
+++ public/wordpress/js/register-form-html.js
@@ -1,7 +1,7 @@
 const register_login_form = `
 <div class="container-fluid tm-mt-60">
         <h2 class="tm-text-primary mb-5">Регистрация/Вход</h2>
-        <form id="contact-form" action="#" method="post" class="tm-contact-form mx-auto">
+        <form id="contact-form" class="tm-contact-form mx-auto">
           <div class="form-group">
             <input type="email" id="email" class="form-control rounded-0" placeholder="Email" required>
           </div>
@@ -9,11 +9,11 @@ const register_login_form = `
             <input type="text" id="password" class="form-control rounded-0" placeholder="Password" required>
           </div>
           <div class="form-group tm-text-right">
-            <button type="submit" class="btn btn-primary">В перед</button>
+            <button type="button" class="btn btn-primary" id="login_button">В перед</button>
           </div>
         </form>
 </div>`
 
-let section_login_page = document.getElementById('section_login')
+document.getElementById('section_login').innerHTML = register_login_form
 
-section_login_page.innerHTML = register_login_form
+
---
+++ public/wordpress/js/register-form-js.js
@@ -0,0 +1,60 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyBoC2t2yfoMY6dAU67HuQerDeygvgK7uIo",
+    authDomain: "skrepeckiy.firebaseapp.com",
+    databaseURL: "https://skrepeckiy-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "skrepeckiy",
+    storageBucket: "skrepeckiy.appspot.com",
+    messagingSenderId: "84562251992",
+    appId: "1:84562251992:web:e820e3303f1221f2ee87ad",
+    measurementId: "G-VZHLXLSYRX"
+}
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
+import { getFirestore, doc, getDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js"
+const app = initializeApp(firebaseConfig)
+const firestore = getFirestore(app)
+const auth = getAuth(app) 
+
+const enter_with_email_and_password = function(pageEmail, pagePassword){
+    signInWithEmailAndPassword(auth, pageEmail, pagePassword).then((userCredential) => {
+        const user = userCredential.user;
+        console.log('uid', user.uid)
+        window.localStorage.setItem('email', pageEmail)
+        window.localStorage.setItem('password', pagePassword)
+        window.localStorage.setItem('uid', user.uid) 
+        console.log(window.localStorage) 
+    }).catch((error) => {git
+        const errorCode = error.code;
+        const errorMessage = error.message;
+    })
+}  
+
+document.getElementById('login_button').addEventListener('click', () =>{
+    let pageEmail    = document.getElementById('email').value
+    let pagePassword = document.getElementById('password').value
+    createUserWithEmailAndPassword(auth, pageEmail, pagePassword).then((userCredential) => {
+        const user = userCredential.user
+        window.localStorage.setItem('email', pageEmail)
+        window.localStorage.setItem('password', pagePassword)
+        window.localStorage.setItem('uid', user.uid)
+        enter_with_email_and_password(pageEmail, pagePassword)
+      }).catch((error) => {
+        const errorCode = error.code
+        const errorMessage = error.message
+        enter_with_email_and_password(pageEmail, pagePassword)       
+      })
+})
+
+push_data_in_form()
+
+function push_data_in_form(){
+  if(localStorage.getItem('email') && localStorage.getItem('password') && localStorage.getItem('uid')){
+    signInWithEmailAndPassword(auth, localStorage.getItem('email'), localStorage.getItem('password')).then((userCredential) => {
+        const user = userCredential.user;
+        document.getElementById('email').value            = localStorage.getItem('email')
+        document.getElementById('password').value         = localStorage.getItem('password')
+        document.getElementById('navbar-brand').innerText = localStorage.getItem('email')
+    })  
+      
+  }
+}
