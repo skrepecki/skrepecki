93e2bce998c880c8a3f966f0025f5ffdeb13fece
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-09-16 00:14:14 +0200

    jijijijjijijiji

--- public/wordpress/js/video.js
+++ public/wordpress/js/video.js
@@ -1,16 +1,21 @@
-var tm_video = document.getElementById('tm-video')
+var tm_video = document.getElementById('video-kuprin')
 
-let current_played = window.localStorage.getItem('play') || 0
 
-if( current_played > 3 ){
-    tm_video.autoplay = false
-    tm_video.style.display = 'none'
-} else {
-    tm_video.autoplay = true
-    tm_video.style.display = 'block'
+document.addEventListener('click', function(){
+    let current_played = window.localStorage.getItem('play') || 0
+
+    if( current_played % 7 == 0 ){
+        tm_video.style.display = 'block'
+        tm_video.play()
+        tm_video.loop = true
+    } else {
+        tm_video.autoplay = false
+        tm_video.style.display = 'none'
+        tm_video.pause()
+    }
+
     current_played++
     window.localStorage.setItem('play', current_played)
-}
 
-
-
+    console.log(current_played)
+})
