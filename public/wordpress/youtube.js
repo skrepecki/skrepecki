87e071041a156373f3faf5cfb5ca7db6b803b069
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-09-15 16:01:21 +0200

    Jijijijjj jijij

--- public/wordpress/youtube.js
+++ public/wordpress/youtube.js
@@ -1,13 +1,10 @@
 let youtube_content = ''
-
 let youtube_array = [
-
+    'hello'
 
 
     
     
 ]
-
 for(let i = 0; i < youtube_array.length; i++) youtube_content += '<iframe width="100%" height="400px" src="'+youtube_array[i]+'" title="Семен Скрепецкий - Семен Скрепецкий Картины - Скрепоносный Бузотер" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
-
 document.getElementById('inframe_video_section').innerHTML = youtube_content
