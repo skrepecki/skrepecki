f0956ddd111a8977c6eb29a711d978b3e5f94672
alexei suzdalenko web android developer <alexei.saron@gmail.com>
2022-09-15 20:14:21 +0200

    jjijjjjjsjjsj

--- public/wordpress/youtube.js
+++ public/wordpress/youtube.js
@@ -1,6 +1,40 @@
-let youtube_content = ''
 let youtube_array = [
+
     "https://www.youtube.com/embed/bpE97y9xryk"
 ]
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+let youtube_content = ''
 for(let i = 0; i < youtube_array.length; i++) youtube_content += '<iframe width="100%" height="400px" src="'+youtube_array[i]+'" title="Семен Скрепецкий - Семен Скрепецкий Картины - Скрепоносный Бузотер" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
 document.getElementById('inframe_video_section').innerHTML = youtube_content
