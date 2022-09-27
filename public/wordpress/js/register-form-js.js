f647147a89a0d3b30fb945164c6988e76b8bf3f8
alexei <alexei.saron@gmail.com>
2022-09-28 00:42:57 +0200

    jijij jjij

--- public/wordpress/js/register-form-js.js
+++ public/wordpress/js/register-form-js.js
@@ -11,54 +11,11 @@ const firebaseConfig = {
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-app.js";
 import { getFirestore, doc, getDoc, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-firestore.js";
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.9.4/firebase-auth.js"
-const app = initializeApp(firebaseConfig)
-const firestore = getFirestore(app)
-const auth = getAuth(app) 
+const Application = initializeApp(firebaseConfig)
+const Firestore = getFirestore(app)
+const Auth = getAuth(app) 
 
-let login_button_click = document.getElementById('login_button')
-if(login_button_click){
-    document.getElementById('login_button').addEventListener('click', () =>{
-        let pageEmail    = document.getElementById('email').value
-        let pagePassword = document.getElementById('password').value
-        createUserWithEmailAndPassword(auth, pageEmail, pagePassword).then((userCredential) => {
-            const user = userCredential.user
-            window.localStorage.setItem('email', pageEmail)
-            window.localStorage.setItem('password', pagePassword)
-            window.localStorage.setItem('uid', user.uid)
-            enter_with_email_and_password(pageEmail, pagePassword)
-          }).catch((error) => {
-            const errorCode = error.code
-            const errorMessage = error.message
-            enter_with_email_and_password(pageEmail, pagePassword)       
-          })
-    })
-}
-
-
-function enter_with_email_and_password(pageEmail, pagePassword){
-    signInWithEmailAndPassword(auth, pageEmail, pagePassword).then((userCredential) => {
-        const user = userCredential.user;
-        window.localStorage.setItem('email', pageEmail)
-        window.localStorage.setItem('password', pagePassword)
-        window.localStorage.setItem('uid', user.uid)
-        window.location.reload() 
-    }).catch((error) => {git
-        const errorCode = error.code;
-        const errorMessage = error.message;
-    })
-}  
-
-push_data_in_form()
-
-function push_data_in_form(){
-    if(localStorage.getItem('email') && localStorage.getItem('password') && localStorage.getItem('uid')){
-        signInWithEmailAndPassword(auth, localStorage.getItem('email'), localStorage.getItem('password')).then((userCredential) => {
-            const user = userCredential.user;
-            document.getElementById('email').value                   = localStorage.getItem('email')
-            document.getElementById('password').value                = localStorage.getItem('password')
-            document.getElementById('navbar-brand').innerText        = localStorage.getItem('email')
-            document.title                                           = localStorage.getItem('email')
-            document.getElementById('form-login-register').innerHTML = '<h4>Вы вошли как ' + localStorage.getItem('email') + '<h4></br></br>'
-        })  
-    }
-}
+window.__Application                    = Application
+window.__Auth                           = Auth
+window.__createUserWithEmailAndPassword = createUserWithEmailAndPassword
+window.__signInWithEmailAndPassword     = signInWithEmailAndPassword
