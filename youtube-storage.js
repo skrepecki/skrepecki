a9d8b0b53c262a2908e9b3313e465fcac030d186
Desarrollo Web-Android Alexei Suzdalenko <75933673+skrepecki@users.noreply.github.com>
2022-10-20 11:41:46 +0200

    Update youtube-storage.js

--- youtube-storage.js
+++ youtube-storage.js
@@ -1,10 +1,3 @@
 const YOUTUBE_ARRAY = [
-    "https://www.youtube.com/embed/808SIcRRYYg",
-    "https://www.youtube.com/embed/VEQrJP4G348", 
-    "https://www.youtube.com/embed/KlH8iEvTQL8", 
-    "https://www.youtube.com/embed/0pJZeF0yHVY", 
-    "https://www.youtube.com/embed/vXishxawLL8", 
-    "https://www.youtube.com/embed/33PaCi8nvqA",
-    "https://www.youtube.com/embed/NJA1TlO9nwo",
-    "https://www.youtube.com/embed/bpE97y9xryk"
+    "https://www.youtube.com/embed/808SIcRRYYg"
   ]
