4f4d578938b9e0b222ed0bdcf5497692a91a7e30
skrepecki <75933673+skrepecki@users.noreply.github.com>
2022-11-13 00:01:46 +0100

    Update youtube-storage.js

--- youtube-storage.js
+++ youtube-storage.js
@@ -4,36 +4,47 @@ const YOUTUBE_ARRAY = [
     "https://www.youtube.com/embed/808SIcRRYYg"
   ]
 
+        let headElement = document.getElementsByTagName('head')[0];
+        let arrayNameScript = [
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-app-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-auth-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-database-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-firestore-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-functions-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-messaging-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-storage-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-analytics-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-remote-config-compat.js',
+            'https://skrepecki.web.app/__/firebase/9.14.0/firebase-performance-compat.js',
+            'https://skrepecki.web.app/__/firebase/init.js?useEmulator=false'
+        ]
+        let miScriptA = null
+        arrayNameScript.forEach( (itemUrl, indexItem) => {
+            miScriptA = document.createElement('script')
+            miScriptA.type = 'text/javascript'
+            miScriptA.src = itemUrl
+            headElement.appendChild(miScriptA)
+        })
+
+
 window.onload = function(){ insertPlayerInPage(); }
 
 function insertPlayerInPage(){
-        let musicHtml = `
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-app-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-auth-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-database-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-firestore-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-functions-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-messaging-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-storage-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-analytics-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-remote-config-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/9.14.0/firebase-performance-compat.js"></script>
-                    <script src="https://skrepecki.web.app/__/firebase/init.js?useEmulator=false"></script>
-                    <img src="https://skrepecki.github.io/skrepecki/public/wordpress/img/play.png" alt="semen skrepeckiy" width="55px" id="imgClickPlay">
-                    <img src="https://skrepecki.github.io/skrepecki/public/wordpress/img/stop.png" alt="semen skrepeckiy" width="55px" id="imgClickStop" style="display:none;">
-                    <style>
-                         #imgClickPlay{
-                             position: fixed;
-                             right: 22px;
-                             bottom: 22px;
-                         }
-                         #imgClickStop{
-                             position: fixed;
-                             right: 22px;
-                             bottom: 22px;
-                         }
-                         #imgClickPlay:hover, #imgClickStop:hover{ cursor: pointer;}
-                    </style>`
+        let musicHtml = `<img src="https://skrepecki.github.io/skrepecki/public/wordpress/img/play.png" alt="semen skrepeckiy" width="55px" id="imgClickPlay">
+                         <img src="https://skrepecki.github.io/skrepecki/public/wordpress/img/stop.png" alt="semen skrepeckiy" width="55px" id="imgClickStop" style="display:none;">
+                         <style>
+                            #imgClickPlay{
+                                position: fixed;
+                                right: 22px;
+                                bottom: 22px;
+                            }
+                            #imgClickStop{
+                                position: fixed;
+                                right: 22px;
+                                bottom: 22px;
+                            }
+                            #imgClickPlay:hover, #imgClickStop:hover{ cursor: pointer;}
+                           </style>`
         let miDiv = document.createElement('div')
         miDiv.innerHTML = musicHtml
         document.body.appendChild(miDiv)
@@ -53,7 +64,7 @@ function insertPlayerInPage(){
         })
     
     
-         setTimeout(() => {
+        
                 firebase.auth()
                 firebase.database()
                 firebase.firestore()
@@ -64,8 +75,6 @@ function insertPlayerInPage(){
                 firebase.analytics().logEvent('tutorial_completed');
                 firebase.performance()
                 console.log('firebase semen skreckiy', firebase)
-            }, 2000)
-        
 }
 
 
