d3ccc08f6754f0b23a9e30706c0006559103e336
СЕМЕН СКРЕПЕЦКИЙ <75933673+skrepecki@users.noreply.github.com>
2023-10-11 17:11:00 +0200

    Update plugins.js

--- public/wordpress/js/plugins.js
+++ public/wordpress/js/plugins.js
@@ -1,3 +1,4 @@
+/*
 function setCannonicalPage(){
   let url = location.href    
   console.log('url', url)
@@ -10,7 +11,7 @@ function setCannonicalPage(){
 }
 setCannonicalPage()
 setInterval(() => setCannonicalPage(), 100)
-
+*/
 
 
 
